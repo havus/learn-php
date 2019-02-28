01b2c3aa19898fd1006da88f87441ccfec8a7481
havus <havus.it@gmail.com>
2019-02-28 21:03:18 +0700

    tambah func

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php 
+// koneksi ke database
+$db = mysqli_connect("localhost", "root", "php123", "phpdasar");
+
+
+function query($query) {
+	global $db;
+	$result = mysqli_query($db, $query);
+	$rows = [];
+	while( $row = mysqli_fetch_assoc($result) ) {
+			$rows[] = $row;
+	}
+	return $rows;
+};
+
+
+
+
+?>
