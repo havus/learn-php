5da9b4d489721777823d9a3d0a87b52ae929c924
havus <havus.it@gmail.com>
2019-03-15 08:04:16 +0700

    func

--- functions.php
+++ functions.php
@@ -124,4 +124,17 @@ function cari($keyword) {
 	return query($query);
 }
 
-?>
+function registrasi($data) {
+	global $conn;
+
+	$username = strtolower(stripslashes($data["username"]));
+	$password =	mysqli_real_escape_string($conn, $data["password"]);
+	$password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+	// cek konfirmasi password
+	if ($password !== $password2) {
+		echo "<script>alert('kopnfirmasi password tidak sesuai!');</script>";
+		return false;
+	}
+}
+
