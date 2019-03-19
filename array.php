92c086ba2b46e37d50a60f2cc78df17a74765509
havus <havus.it@gmail.com>
2019-03-19 18:02:38 +0700

    array learn

--- array.php
+++ array.php
@@ -1,10 +1,22 @@
 <?php 
-$conn = mysqli_connect("localhost", "root", "php123", "phpdasar");
+$conn = mysqli_connect("localhost", "root", "php123", "praktek");
 
+$array = [  "nama" => "hafidz",
+            "alamat" => "pinggir jalan gede",
+            "hobi" => "makan" 
+];
 
 $username = 'havus';
-$result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+$result = mysqli_query($conn, "SELECT * FROM wisata");
 
-var_dump(mysqli_fetch_assoc($result));
+var_dump( mysqli_fetch_row( $result ) );
+
+foreach( $result as $row ) {
+    print_r($row);
+    echo "<br>";
+}
 
 // kalo null berarti array(data) belum ada di database;
+
+// mysqli_num_rows()
+print_r(mysqli_num_rows($result));
